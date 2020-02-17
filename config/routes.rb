cae09c80429161412ede2d55e8a2b190f3bbd421
hugo mori <mori.hugo@outlook.com>
2020-02-17 17:08:43 +0100

    Modif erreur

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#index'
+  root to: 'palaces#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :palaces do
     resources :bookings, only: [:new, :create]
