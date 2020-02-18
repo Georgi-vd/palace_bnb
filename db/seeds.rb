ff73607ae5018fa81cdc017feb5b536f93fd9ec6
Emilie Keller <emilie.keller@bluewin.ch>
2020-02-18 10:59:43 +0100

    début du front-end (index, navbar)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Palace.destroy_all
+
+puts 'Creating user'
+user = User.create!(email: 'bob@bob.com', password: 'xxxxxxxx')
+puts 'Creating palaces...'
+20.times do
+  palace = Palace.new(
+    name: Faker::Superhero.name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Number.leading_zero_number(digits: 10),
+    location: Faker::Address.country,
+    user: user
+  )
+  palace.save!
+end
+puts 'Finished!'
